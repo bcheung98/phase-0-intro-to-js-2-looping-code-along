5a37971e1dfe856b5071ebbce72228b727e63cb6
Brian <briantcheung@gmail.com>
2021-02-13 00:57:10 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
